23a68fb7b76aa48fd6d142fabaed6f729b4c78c9
lish <shuzhou.li@ssfs.org>
2019-09-12 08:57:49 -0400

    escape character

---
+++ src/com/company/Meta.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Meta {
+    public static void main (String[] args){
+        System.out.print("Hello");
+        System.out.print(", ");
+        System.out.print("world");
+        System.out.print("!");
+    }
+}
