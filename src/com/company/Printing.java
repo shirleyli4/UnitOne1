584f26ad8cb553e68d02042cb4bbe0c6775e8d4c
Shuzhou Li <lish@ssfs.org>
2019-09-12 08:28:00 -0400

    Initial commit

---
+++ src/com/company/Printing.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Printing {
+
+    public static void Main (String[] args) {
+	// write your code here
+        System.out.println("8:22");
+        System.out.println("09/12/19");
+        System.out.println("It is a great ");
+        System.out.println("time to be a ");
+        System.out.println("computer programmer!");
+        System.out.print("It is a great ");
+        System.out.print("time to be a ");
+        System.out.print("computer programmer!");
+        
+    }
+}
