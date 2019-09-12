70b368d96c49da919b985fb65fabc32cc7484b82
lish <shuzhou.li@ssfs.org>
2019-09-12 08:50:37 -0400

    escape character

---
+++ src/com/company/Escape.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Escape {
+    public static void main (String[] args){
+        System.out.println("What is the difference between");
+        System.out.println("a' and a\"? Or between a\" and a\\\"?");
+        System.out.println("One is what we see we're typing our program.");
+        System.out.println("The other is what appears on the \"console.\"");
+        System.out.println("  \\/");
+        System.out.println(" \\\\//");
+        System.out.println("\\\\\\///");
+        System.out.println("///\\\\\\");
+        System.out.println(" //\\\\");
+        System.out.println("  /\\");
+
+
+
+
+    }
+}
