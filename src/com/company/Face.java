9ee3624cd7e5c979a09fadc97ffff16689666da6
lish <shuzhou.li@ssfs.org>
2019-09-13 09:30:23 -0400

    escape character

---
+++ src/com/company/Face.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Face {
+    public static void main (String[] args){
+        System.out.println("  xxxxx  ");
+        System.out.println("  x   x ");
+        System.out.println("(( o o ))");
+        System.out.println(" |  V  | ");
+        System.out.println(" \\ === /");
+        System.out.println("  -----  ");
+    }
+}
