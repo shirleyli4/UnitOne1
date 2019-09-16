1444c05a1e271e6f32a0fccb0e2781a5015284d9
lish <shuzhou.li@ssfs.org>
2019-09-16 11:05:07 -0400

    temp covert

---
+++ src/com/company/TempConversion.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class TempConversion {
+    public static void main (String[] args){
+        double fahren = 586;
+        double celsius = fahren / 1.8 - 32;
+        System.out.println(fahren + " Fahrenheit equals "+ celsius +" Celsius.");
+    }
+}
