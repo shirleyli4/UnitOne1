1140aa2625e9f03cba261b26e04ab5f89d8effc6
lish <shuzhou.li@ssfs.org>
2019-09-16 10:44:45 -0400

    rectangle area

---
+++ src/com/company/RectangleArea.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class RectangleArea {
+    public static void main (String[] args) {
+        int length = 14;
+        int width = 6;
+        int area = length * width;
+        System.out.println("the area is " + area);
+    }
+}
