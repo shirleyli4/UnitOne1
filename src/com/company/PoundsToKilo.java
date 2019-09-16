1e269e1282c4d50a4123a1bb3c54415306306916
lish <shuzhou.li@ssfs.org>
2019-09-16 10:50:00 -0400

    rectangle area

--- src/com/company/PoundsToKilo.java
+++ src/com/company/PoundsToKilo.java
@@ -4,6 +4,7 @@ public class PoundsToKilo {
     public static void main (String[] args){
         double lbs = 115;
         double kg = lbs/2.205;
+        System.out.println(lbs + " pounds"+ " = "+ kg+ " kilograms");
 
     }
 }
