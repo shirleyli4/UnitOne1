5dd96b11c3fd4255d42ef9f824542f4e083b0c04
lish <shuzhou.li@ssfs.org>
2019-09-17 12:36:41 -0400

    pounds to kilo

--- src/com/company/PoundsToKilo.java
+++ src/com/company/PoundsToKilo.java
@@ -1,8 +1,10 @@
 package com.company;
-
+import java.util.Scanner;
 public class PoundsToKilo {
     public static void main (String[] args){
-        double lbs = 115;
+        Scanner theInput = new Scanner(System.in);
+        System.out.println("Please enter the number of pounds");
+        double lbs = theInput.nextDouble();
         double kg = lbs/2.205;
         System.out.println(lbs + " pounds"+ " = "+ kg+ " kilograms");
 
