ff9f6cb1d4c24f897d969ec795b50b2295019efd
lish <shuzhou.li@ssfs.org>
2019-09-17 12:38:23 -0400

    temp

--- src/com/company/TempConversion.java
+++ src/com/company/TempConversion.java
@@ -1,8 +1,10 @@
 package com.company;
-
+import java.util.Scanner;
 public class TempConversion {
     public static void main (String[] args){
-        double fahren = 586;
+        Scanner theInput = new Scanner(System.in);
+        System.out.println("Please enter the number of the fahrenheit");
+        double fahren = theInput.nextDouble();
         double celsius = (fahren - 32)/1.8;
         System.out.println(fahren + " Fahrenheit equals "+ celsius +" Celsius.");
     }
