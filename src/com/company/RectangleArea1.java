d6ad700f2cf02aa474f2d9a34858bf660e7016f6
lish <shuzhou.li@ssfs.org>
2019-09-17 12:33:20 -0400

    rectangle area

--- src/com/company/RectangleArea1.java
+++ src/com/company/RectangleArea1.java
@@ -1,10 +1,13 @@
 package com.company;
-
+import java.util.Scanner;
 public class RectangleArea1 {
     public static void main (String[] args) {
-        int length = 14;
-        int width = 6;
-        int area = length * width;
+        Scanner theInput = new Scanner(System.in);
+        System.out.println("Please enter the length of the rectangle");
+        double length = theInput.nextDouble();
+        System.out.println("Please enter the width of the rectangle");
+        double width = theInput.nextDouble();
+        double area = length * width;
         System.out.println("Area = " + area);
     }
 }
