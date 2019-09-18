abdfe078ae0dbc7c2a445fc1ca01235600ebfc70
lish <shuzhou.li@ssfs.org>
2019-09-18 13:45:05 -0400

    get 1 take 1

---
+++ src/com/company/AddOneTakeOne.java
@@ -0,0 +1,18 @@
+package com.company;
+import java.util.Scanner;
+public class AddOneTakeOne {
+    public static void main (String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = input.nextInt();
+        num--;
+        System.out.print(num +" ");
+        num++;
+        System.out.print(num +" ");
+        num++;
+        System.out.print(num +" ");
+
+
+
+    }
+}
