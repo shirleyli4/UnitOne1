330a2443017109eac001724fa1dafb7fde405523
lish <shuzhou.li@ssfs.org>
2019-09-19 15:07:04 -0400

    round 10

--- src/com/company/RoundTenth.java
+++ src/com/company/RoundTenth.java
@@ -7,7 +7,11 @@ public class RoundTenth {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a decimal number please!");
         double decinum = input.nextDouble();
-        double after =((int) ((decinum - (int)decinum)*10+0.5))/10;
-        System.out.println(decinum+" rounded to the nearest tenth is "+ after);
+        double after =(int) ((decinum - (int)decinum)*10+0.5);
+        double afterafter = after/10;
+        double decinum1 =(int)(decinum)+afterafter;
+        System.out.println(decinum+" rounded to the nearest tenth is "+ decinum1);
+
+
     }
 }
